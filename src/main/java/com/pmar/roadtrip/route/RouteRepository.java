011837d79d802d7dcc9d3f0a488c72f999474f1b
Patrick Markowski <PMARKOWS@depaul.edu>
2023-07-20 23:10:41 -0700

    Added optional JPA interface method to extract all routes under one specific userID

--- src/main/java/com/pmar/roadtrip/route/RouteRepository.java
+++ src/main/java/com/pmar/roadtrip/route/RouteRepository.java
@@ -2,5 +2,10 @@ package com.pmar.roadtrip.route;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RouteRepository extends JpaRepository<Route,Long> {
+    Optional<List<Route>> findAllByUserId(Long userId);
+
 }
